37e9b63d05e8157fa37a3acb925a27eacf6e4f9b
locnguyen3108 <loclieulinh318@gmail.com>
2024-05-17 23:25:39 +0700

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/crm-procast/',
+  base: '',
   plugins: [react()],
   server: {
     port: 3000
